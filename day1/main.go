45e279f82badbf91a09fcb4a3ddd8c8aff529409
Røysland, Tor Edvard <TorEdvard.Roysland@skatteetaten.no>
2024-12-04 12:46:08 +0100

    yeet and yoot

---
+++ day1/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// Open the file for reading
+	file, err := os.Open("foo.txt")
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+
+	var yeetStr []string
+	var yootStr []string
+	var yeetInt []int
+	var yootInt []int
+	// Loop through each line in the file
+	for scanner.Scan() {
+		// Get the current line from the scanner
+		line := scanner.Text()
+
+		parts := strings.Split(line, "   ")
+
+		fmt.Println("splitt line:", parts)
+
+		for i, part := range parts {
+			if i == 0 {
+				fmt.Println("parts:", part)
+				yeetStr = append(yeetStr, part)
+			} else {
+				fmt.Println("parts:", part)
+				yootStr = append(yootStr, part)
+			}
+		}
+	}
+	fmt.Println("list yeetStr", yeetStr)
+	fmt.Println("list yootStr", yootStr)
+
+	for _, str := range yeetStr {
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			fmt.Println("Error converting yeet-string to int:", err)
+			continue
+		}
+		yeetInt = append(yeetInt, num)
+	}
+
+	for _, str := range yootStr {
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			fmt.Println("Error converting yoot-string to int:", err)
+			continue
+		}
+		yootInt = append(yootInt, num)
+	}
+
+	sort.Ints(yootInt)
+	sort.Ints(yeetInt)
+	fmt.Println("list yeetInt sorted", yeetInt)
+	fmt.Println("list yootInt sorted", yootInt)
+
+	var sum int
+	for i, yoo := range yootInt {
+		for j, yee := range yeetInt {
+			if i == j {
+				if yee > yoo {
+					sum += yee - yoo
+				} else {
+					sum += yoo - yee
+				}
+			}
+		}
+	}
+	fmt.Println("finnal diff", sum)
+
+	// Check for errors that may have occurred during the scan
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+}
